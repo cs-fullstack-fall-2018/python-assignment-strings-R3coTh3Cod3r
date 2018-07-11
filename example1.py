8590adbe431308db014cfecce27b110328f9b5a2
Mareco Nabors <naborsmarico99@gmail.com>
2018-07-11 15:35:56 -0500

    Checkin of execercise

---
+++ example1.py
@@ -0,0 +1,22 @@
+import os
+os.system('clear')
+
+bandMate = "riNgo"
+
+print("The name is this long", len(bandMate))
+# 1
+x = bandMate
+print(bandMate.upper())
+print(bandMate.lower())
+print(bandMate[2])
+print(bandMate[1:4])
+print(bandMate[3:])
+
+# # 2
+# x= bandMate
+# print(bandMate.capitalize())
+# print(bandMate.swapcase())
+# print(bandMate[2])
+# print(bandMate[1:4])
+# print(bandMate[3:])
+
